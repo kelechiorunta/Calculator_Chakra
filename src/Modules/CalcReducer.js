b96c667a76128f5a424a4c7d87625ad3ff038e7d
kelechiorunta <>
2024-03-25 10:26:32 +0100

    Recent Update on the Calculator Reducer Fuction

--- src/Modules/CalcReducer.js
+++ src/Modules/CalcReducer.js
@@ -2,7 +2,7 @@ export const CalcReducer = (state, action) =>{
     switch(action.type){
         case "APPEND":{
             return{...state, digit: (state.digit==="0" || state.operatorClicked===true)? 
-                            Number(action.payload): Number(state.digit + action.payload), 
+                            Number(action.payload): (state.digit.toString().includes(".") && action.payload==="0")? state.digit + action.payload:Number(state.digit + action.payload), 
                             operator: state.operator, operatorClicked: false}
         }
         case "RESET":{
@@ -14,6 +14,10 @@ export const CalcReducer = (state, action) =>{
         case "DELETE":{
             return{...state, digit: Number(state.digit.toString().substr(0, state.digit.toString().length - 1)),
                              operator:state.operator, operatorClicked:state.operatorClicked}
+        }
+        case "ADD_DECIMAL":{
+            return{...state, digit: state.operatorClicked===true?"0.":(state.digit.toString().includes("."))? (state.digit.toString()) : state.digit+".",
+                             operator: state.operator, operatorClicked: false}
         }
         default:{
             return state
